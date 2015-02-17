a98bdc98db2c85adceb1eda9b0d17a01b97647c9
Slava Bacherikov <slava@bacher09.org>
2015-02-17 13:37:29 +0200

    Add support for secure time rcon

--- src/DarkPlaces/Rcon.hs
+++ src/DarkPlaces/Rcon.hs
@@ -1,6 +1,8 @@
 module DarkPlaces.Rcon where
 import Crypto.Hash
+import Data.Byteable
 import Data.Monoid
+import Text.Printf
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Builder as BB
 import qualified Data.ByteString.Lazy as BL
@@ -8,6 +10,10 @@ import qualified Data.ByteString.Lazy as BL
 
 quakePacketHeader :: B.ByteString
 quakePacketHeader = B.pack [0xFF, 0xFF, 0xFF, 0xFF]
+
+
+hmacMD4 :: B.ByteString -> B.ByteString -> B.ByteString
+hmacMD4 secret msg = toBytes (hmac secret msg :: HMAC MD4)
 
 
 rconNonSecurePacket :: B.ByteString -> B.ByteString -> BL.ByteString
@@ -18,3 +24,16 @@ rconNonSecurePacket passw command = BB.toLazyByteString builder
                        BB.byteString passw,
                        BB.char7 ' ',
                        BB.byteString command]
+
+
+rconSecureTimePacket :: (Fractional a, PrintfArg a) => a -> B.ByteString -> B.ByteString -> BL.ByteString
+rconSecureTimePacket t passw command = BB.toLazyByteString builder
+  where
+    time_str = BB.string7 $ printf "%.6f" t
+    time_command = mconcat [time_str, BB.char7 ' ', BB.byteString command]
+    time_command_str = BL.toStrict $ BB.toLazyByteString time_command
+    key = BB.byteString $ hmacMD4 passw time_command_str
+    builder = mconcat [BB.byteString quakePacketHeader,
+                       BB.string7 "srcon HMAC-MD4 TIME ",
+                       key, BB.char7 ' ',
+                       BB.byteString time_command_str]
