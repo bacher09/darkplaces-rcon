df88b0c619d9d9dea0cf42e966fa1eb717214e72
Slava Bacherikov <slava@bacher09.org>
2015-02-18 15:17:44 +0200

    Change time data type

--- src/DarkPlaces/Rcon.hs
+++ src/DarkPlaces/Rcon.hs
@@ -95,10 +95,10 @@ rconNonSecurePacket passw command = BB.toLazyByteString builder
                        BB.byteString command]
 
 
-rconSecureTimePacket :: (Fractional a, PrintfArg a) => a -> B.ByteString -> B.ByteString -> BL.ByteString
+rconSecureTimePacket :: (Real a) => a -> B.ByteString -> B.ByteString -> BL.ByteString
 rconSecureTimePacket t passw command = BB.toLazyByteString builder
   where
-    time_str = BB.string7 $ printf "%.6f" t
+    time_str = BB.string7 $ printf "%.6f" $ (realToFrac t :: Double)
     time_command = mconcat [time_str, BB.char7 ' ', BB.byteString command]
     time_command_str = BL.toStrict $ BB.toLazyByteString time_command
     key = BB.byteString $ hmacMD4 passw time_command_str
